d0866715b4743d4080aa52c0d1672355955d4322
Muhammad Rahmatullah <rahmatthea5@gmail.com>
2019-12-21 00:02:49 +0700

    permit only related link

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -69,6 +69,6 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:related_link, :description, :title, :sub_title, :current_price, :previous_price)
+      params.require(:product).permit(:related_link)
     end
 end
