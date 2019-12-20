cd41980a0812af95d94650814c1b6691304591db
Muhammad Rahmatullah <rahmatthea5@gmail.com>
2019-12-20 14:49:57 +0700

    add reference on migration

--- db/migrate/20191220074732_create_product_images.rb
+++ db/migrate/20191220074732_create_product_images.rb
@@ -2,7 +2,7 @@ class CreateProductImages < ActiveRecord::Migration[6.0]
   def change
     create_table :product_images do |t|
       t.string :url
-
+      t.references :product
       t.timestamps
     end
   end
