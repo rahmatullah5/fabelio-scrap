6488e076ac90dd3a3d877782553d96dc4b1f42af
Muhammad Rahmatullah <rahmatthea5@gmail.com>
2019-12-22 13:30:39 +0700

    remove unused route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'products#new'
-  resources :products
+  resources :products, except: [:update]
   devise_for :admins
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
